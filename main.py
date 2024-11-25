80228b17a584004f2c76196f8b297a255379ae9e
Ay-Aal <88855746+Cynosure11@users.noreply.github.com>
2024-11-25 11:47:12 -0500

    Create columns, image and description

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", width=300)
+with col2:
+    st.title("Aial Atlasov")
+    content = """
+    Hi, I am Aial! I am a Python programmer and founder of AI Sport Solution
+    """
+    st.info(content)
