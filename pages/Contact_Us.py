fcda06aff83c25d140bc055e0f29d74c66002c99
Ay-Aal <88855746+Cynosure11@users.noreply.github.com>
2024-12-30 15:40:50 -0500

    I believe this project is ready to go

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,21 +1,31 @@
+from pydoc_data.topics import topics
+
 import streamlit as st
 from send_email import send_email
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
 
 st.header("Contact Us")
 
 with st.form(key="email_forms"):
-    user_email = st.text_input("Enter your email address")
-    raw_message = st.text_area("Enter your message")
+    user_email = st.text_input("Your email address")
+    option = st.selectbox(
+        "What topic do you want to discuss?",
+        df["topic"]
+    )
+    raw_message = st.text_area("Text")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email} 
+Topic: {option}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
     print(button)
     if button:
         send_email(message)
-        st.info("Email Sent Successfully")
+        st.info("Your email was sent successfully")
 
 
